605452e3a991fabf74bc9e6d650fb339a98d9f08
DESKTOP-AM8FIS3\CuongNV <cuong.nguyen.intern@ntq-solution.com.vn>
2018-06-26 17:47:03 +0700

    commit

---
+++ Js/Gui.js
@@ -0,0 +1,331 @@
+var VisualizeGui = {
+	themes : "",
+	// music setting
+		playlists : "",
+		songs : "",
+		deleteThisSong : function(){
+			deleteCurrentObjectInList(dropListMusic, SongList, VisualizeGui.songs);
+		},
+		clearSongs : function(){
+			updateDropDown(dropListMusic, []);
+			SongList = [];
+			indexSongNow = 0;
+		},
+		loop: false,
+		rand: false,
+		volume : 1,
+		saveList : function() {
+			var nameList = prompt("Name of Playlist: ");			
+			saveJSON({nameList, SongList}, nameList+'-playlist');
+		},
+
+	// background setting
+		backgs : "",
+		linkBack : "https://static.pexels.com/photos/355465/pexels-photo-355465.jpeg",
+		loadLinkBack: function(){
+			if(VisualizeGui.linkBack !== "")
+			loadImage(VisualizeGui.linkBack,
+				function(data){
+					backG = data;
+					var name = "Background "+(BackList.length+1);
+					BackList.push({"name":name, "link":VisualizeGui.linkBack});
+					backgNow = BackList.length-1; 
+					addToDropdown(dropListBackG, name);
+					VisualizeGui.backgs = name;
+				},
+				function(e){
+					alert('can not load this image link\n'+e);
+				});
+		},
+		autoChangeBack : false,
+		animateBack : true,
+		deleteThisBack : function(){
+			deleteCurrentObjectInList(dropListBackG, BackList, VisualizeGui.backgs);
+		},
+		clearBacks : function(){
+			updateDropDown(dropListBackG, []);
+			BackList = [];
+			backgNow = 0;
+		},
+
+	// focus
+		checkFocus : true,
+		whatthis_checkFocus : function(){
+			alert(`if you turn on this mode, the visualyze will 
+                 not refresh screen (redraw) IF user NOT FOCUS in this WEB`);
+		},
+
+	// connect to all audio source
+		fromMic : false,
+		whatthis_fromMic : function(){
+			alert(`use your microphone like input source and visualize it`);
+		},
+
+	// link to current song
+		linkCurrentSong : "",
+		openLink : function(){
+			window.open(VisualizeGui.linkCurrentSong);
+		},
+
+	// visualize folder
+		showDesignMode: false,
+		ampType : "lineGraph",
+		add_amp : function(){
+			var type = VisualizeGui.ampType;
+			objects.push(new AmplitudeGraph(random(width), random(height), 100, 100, type));
+		},
+
+		fftType : "center",
+		add_fft : function(){
+			var type = VisualizeGui.fftType;
+			objects.push(new fftGraph(random(width), random(height), 200, 100, type));
+		},
+
+	// buttons folder
+		add_playBut : function(){
+			objects.push(new ButtonShape(random(width), random(height), 
+				120, 60, "Play", function(){animationAvatar();},  function(){playPause();}));
+		},
+		add_nextBut: function(){
+			objects.push(new ButtonShape(random(width), random(height), 
+				70, 50, "Next", null, function(){nextPre('next');}));
+		},
+		add_preBut : function(){
+			objects.push(new ButtonShape(random(width), random(height), 
+				70, 50, "Pre", null, function(){nextPre('pre');}));
+		},
+	// time
+		timeColor: "#ffffff",
+		add_time : function(){
+			objects.push(new textBox(width/2, height/2, 100, 20, null, 'time'));
+		},
+
+	// title
+		titleColor: "#ffae23",
+		titleName : "",
+		add_titleSong : function(){
+			objects.push(new textBox(width/2, 100, 100, 25, info.title, 'title'));
+		},
+
+	// text
+		textValue : "write your text here",
+		textColor: "#ffae23",
+		add_text : function(){
+			objects.push(new textBox(100, height-100, 100, 25, VisualizeGui.textValue, 'text'));	
+		},
+
+	// lyric
+		lyricColor : "#ffae23",
+		lyricColor2: "#ffae23",
+		add_lyric : function(){
+			objects.push(new textBox(width/2, height/2, 100, 25, VisualizeGui.textValue, 'lyric'));	
+		},
+
+	// save theme
+		savetheme : function(){
+			saveTheme();
+		},
+
+	// share
+		shareSong : function(){
+			if(getPlaylistIndex(VisualizeGui.playlists)+1 > 50)
+				alert('Can not share song from USER PLAYLIST , please choose another playlist and try again');
+			else{
+				var linkShare = "https://hoangtran0410.000webhostapp.com/Visualyze%20Design/";
+				linkShare += ("?theme="+(VisualizeGui.themes||"HauMaster"));
+				linkShare += ("&playlist="+(getPlaylistIndex(VisualizeGui.playlists)+1));
+				linkShare += ("&song="+(indexSongNow+1));
+				// linkShare += ("&linksong="+SongList[indexSongNow].link);
+				linkShare += ("&background="+(backgNow+1));
+
+				prompt("Ctrl+C to copy: ", linkShare);
+			}
+		},
+		shareTheme : function(){
+			var linkShare = "https://hoangtran0410.000webhostapp.com/Visualyze%20Design/";
+			linkShare += "?customtheme="+saveTheme(true);
+			console.log(linkShare);
+			prompt("Ctrl+C to copy: ", linkShare);
+		},
+
+	// help
+		help : function(){
+			help();
+		},
+
+	//About
+		// github
+		github : function(){
+			window.open('https://github.com/HoangTran0410/Visualyze-design-your-own-'); 
+		},
+
+		// facebook
+		fb: function () {
+			window.open('https://www.facebook.com/people/Hoang-Tran/100004848287494');
+		},
+
+		// old version
+		old : function(){
+			window.open('https://hoangtran0410.github.io/VisualyzeTest/');
+		}
+};
+
+function addGui(){
+	gui = new dat.GUI({width:350});
+
+	var setting = gui.addFolder('Setting');
+
+	var audioSetting = setting.addFolder('Audio');
+		dropPlaylists = audioSetting.add(VisualizeGui, 'playlists', [])
+			.name('Playlist')
+			.onChange(function(value){getPlaylist(value);}).listen();
+		dropListMusic = audioSetting.add(VisualizeGui, 'songs', [])
+			.name('List music')
+			.onChange(function(value){playMusicFromName(value)}).listen();
+		audioSetting.add(VisualizeGui, 'volume', 0, 1).step(0.01).name('Volume').listen()
+			.onChange(function(value){myAudio.elt.volume = value;});
+		audioSetting.add(VisualizeGui, 'loop').name('Loop song');
+		audioSetting.add(VisualizeGui, 'rand').name('Random');
+
+		var customListMusic = audioSetting.addFolder('Custom List Music');
+			customListMusic.add(VisualizeGui, 'deleteThisSong').name('Delete this song');	
+			customListMusic.add(VisualizeGui, 'clearSongs').name('Clear List Music');
+			customListMusic.add(VisualizeGui, 'saveList').name('Save List Music').listen();
+
+		var dev = audioSetting.addFolder('Soucloud, ZingMp3');
+			dev.add(DEV, 'linkSC').name('Link Soundcloud');
+			dev.add(DEV, 'loadSC').name('Load SC');
+			dev.add(DEV, 'IDzing').name('ID zingmp3');
+			dev.add(DEV, 'loadId').name('Load id');
+			dev.add(DEV, 'howtoLoadID').name('How to get ID');
+			dev.add(DEV, 'linkmedia').name('Link media');
+			dev.add(DEV, 'load').name('Load');
+
+	var backSetting = setting.addFolder('Background');
+		dropListBackG = backSetting.add(VisualizeGui, 'backgs',[])
+			.name('Background').listen().onChange(function(value){
+						applyBackground(value);
+					});
+		backSetting.add(VisualizeGui, 'linkBack').name('Background Link');
+		backSetting.add(VisualizeGui, 'loadLinkBack').name('Load Link');
+		backSetting.add(VisualizeGui, 'autoChangeBack').name('b.g AutoChange')
+			.onChange(
+				function(value){
+					if(value) {
+						autoChangeBackStep = prompt("Please enter step (in seconds):", "15");
+						changeRandom = confirm("change random or not ? press OK to change random");
+					}
+					else autoChangeBackStep = 0;
+				});
+		backSetting.add(VisualizeGui, 'animateBack').name('b.g Animation ');
+
+		var customListBack = backSetting.addFolder('Custom List Background');
+			customListBack.add(VisualizeGui, 'deleteThisBack').name('Delete this b.g');
+			customListBack.add(VisualizeGui, 'clearBacks').name('Clear B.G List');
+
+	var more = setting.addFolder('More');
+		var connectMic = more.addFolder('Mic_Input');
+			connectMic.add(VisualizeGui, 'fromMic').name('turn on')
+					.onChange(function(value){
+						if(value) {
+							mic.start(); FftData.setInput(mic); AmpData.setInput(mic);}
+						else {mic.stop(); FftData.setInput(myAudio);AmpData.setInput(myAudio);}
+					});
+			connectMic.add(VisualizeGui, 'whatthis_fromMic').name('What is this');
+		var weakPc = more.addFolder('For weak PC');
+			weakPc.add(VisualizeGui, 'checkFocus').name('only Run If Focus');
+			weakPc.add(VisualizeGui, 'whatthis_checkFocus').name('What is this');
+		var linkCurrentS = more.addFolder('Link this song');
+			linkCurrentS.add(VisualizeGui, 'linkCurrentSong').name('Link').listen();
+			linkCurrentS.add(VisualizeGui, 'openLink').name('Open in new tab');
+
+	dropThemes = setting.add(VisualizeGui, 'themes', ['HauMasterLite', 'HauMaster', 'HoangTran'])
+		.listen()
+		.name('Themes')
+		.onChange(function(value){
+			loadJSON('default theme/'+value+'.json',
+				// loaded
+				function(data){
+					loadTheme(data, false, true);
+				},
+				// error
+				function(){
+					alert('can"t load this theme');
+				}
+			);
+		});
+
+	var design = gui.addFolder("Design")
+		design.add(VisualizeGui, 'showDesignMode').name('Design mode').listen()
+			.onChange(function(value){designMode = value;});
+		var ampFolder = design.addFolder('Amplitude');
+			ampFolder.add(VisualizeGui, 'ampType', ["lineGraph","circle", "singleRect", "singleRect_Ngang"]).name('Amp Type');
+			ampFolder.add(VisualizeGui, 'add_amp').name('Add Amp');
+		var fftFolder = design.addFolder('FFT');
+			fftFolder.add(VisualizeGui, 'fftType', ["center", "center noColor", "bottom", "bottom noColor","circle", "circle moving", "waveform"]).name('FFT Type');
+			fftFolder.add(VisualizeGui, 'add_fft').name('Add FFT');
+		var buts = design.addFolder('Buttons');
+			buts.add(VisualizeGui, 'add_playBut').name('Play button');
+			buts.add(VisualizeGui, 'add_nextBut').name('Next button');
+			buts.add(VisualizeGui, 'add_preBut').name('Pre button');
+		var title = design.addFolder('Title');
+			title.addColor(VisualizeGui, 'titleColor').name('Title Color').listen();
+			title.add(VisualizeGui, 'titleName').name('Custom Text').listen()
+				.onChange(function(value){info.title = value;});
+			title.add(VisualizeGui, 'add_titleSong').name('Add Title');
+		var timeSong = design.addFolder('Time');
+			timeSong.addColor(VisualizeGui, 'timeColor').name('Color').listen();
+			timeSong.add(VisualizeGui, 'add_time').name('Add Time');
+		var textbox = design.addFolder('Text Box');
+			textbox.add(VisualizeGui, 'textValue').name('Your Text');
+			textbox.addColor(VisualizeGui, 'textColor').name('Textbox Color').listen();
+			textbox.add(VisualizeGui, 'add_text').name('Add Text');
+		var lyric = design.addFolder('Lyric');
+			lyric.addColor(VisualizeGui, 'lyricColor').listen();
+			lyric.addColor(VisualizeGui, 'lyricColor2').listen();
+			lyric.add(VisualizeGui, 'add_lyric').name('Add Lyric');
+		design.add(VisualizeGui, 'savetheme').name('Save Theme');
+
+	var sharing = gui.addFolder('Share');
+		sharing.add(VisualizeGui, 'shareSong').name("Share this song");
+		sharing.add(VisualizeGui, 'shareTheme').name("Share custom theme");
+
+	var about = gui.addFolder('About');
+		about.add(VisualizeGui, 'github').name('My github');
+		about.add(VisualizeGui, 'fb').name('My Facebook');
+		about.add(VisualizeGui, 'old').name('Old Version');
+		
+	gui.add(VisualizeGui, 'help').name('Help');
+
+	updateDropDown(dropPlaylists, PlayList);
+	updateDropDown(dropListMusic, SongList);
+	updateDropDown(dropListBackG, BackList);
+}
+
+var DEV = {
+	linkSC: "https://soundcloud.com/levipatel/as-she-passes",
+	loadSC : function(){
+		addAudio(DEV.linkSC);
+	},
+
+	linkmedia: `http://stream.radioreklama.bg:80/radio1128`,
+	load : function(){
+		createNewAudio(DEV.linkmedia);
+	},
+	howtoLoadID : function(){
+		window.open('https://hoangtran0410.github.io/Tools/get%20ID%20zingmp3%20Song/');
+	},
+
+	IDzing: "ZmxmyLmsckblnkFymybmZHyLWDhBCvJDN",
+	loadId : function(){
+		addAudio(DEV.IDzing);
+	}
+}
+
+function getPlaylistIndex(name){
+	for(var i = 0; i < PlayList.length; i++){
+		if(name == PlayList[i].name){
+			return i;
+		}
+	}
+}
---
+++ Js/Sketch.js
@@ -0,0 +1,270 @@
+var myAudio;
+var dropPlaylists;
+var dropListMusic;
+var dropListBackG;
+var dropThemes;
+var indexSongNow;
+var info;
+
+var backG;
+var backgNow;
+var objects = [];
+
+var mic;
+var AmplitudeData;
+var FftData;
+var ampLevel;
+var fftAnalyze;
+var fftWave;
+
+var rectChooseMulti;
+var designMode = false;
+var mouseActive = 0; // when mouse not move after 5s -> close dat.gui
+var preWidth, preHeight;
+var client_id = '587aa2d384f7333a886010d5f52f302a'; // Soundcloud
+var gui;
+
+var songFromLocation = {
+	haveSong : false,
+	indexSong : 0
+}
+
+function setup() {
+	// first setting
+	createCanvas(windowWidth, windowHeight).smooth().position(0, 0).drop(getFileLocal);
+	colorMode(HSB);
+	angleMode(DEGREES);
+	imageMode(CENTER);
+	rectMode(CENTER);
+	textSize(20);
+	textAlign(CENTER, CENTER);
+	
+	preWidth = width;
+	preHeight = height;
+	mic = new p5.AudioIn();
+	AmpData = new p5.Amplitude();
+	FftData = new p5.FFT(0.4, 1024);
+	
+	// add object
+	info = new InfoSong();
+	rectChooseMulti = new rectChooseMultiObject();
+	addGui();
+
+	// from href
+	var l = window.location.href;
+	if(l.search("[?]") > 0){
+		l = l.substring(l.search("[?]")+1);
+		var check = l.substring(0, l.search("[=]"));
+		if(check == 'customtheme'){
+			l = l.substring(l.search("[=]")+1)
+				.replace(/\%20|\+/gi, " ")
+				.replace(/\%22/gi, "\"")
+				.replace(/\%7B/gi, "{")
+				.replace(/\%7D/gi, "{")
+				.replace(/\%5B/gi, "[")
+				.replace(/\%5D/gi, "]");
+
+			var  data = JSON.parse(l);
+			loadTheme(data, false, true, true);
+
+			// VisualizeGui.playlists = PlayList[floor(random(PlayList.length))].name;
+			// getPlaylist(VisualizeGui.playlists);
+		}
+		else while(l.search("[=]") > 0){
+			var sch = l.search("[&]");
+			var left = l.substring(0, l.search("[=]"));
+			var right = l.substring(l.search("[=]")+1, (sch>0)?sch:l.length);
+			if(sch > 0) l = l.substring(sch+1);
+			else l = "";
+
+			console.log(left+'='+right);
+
+			switch(left){
+				case "theme":
+					VisualizeGui.themes = right;
+					loadJSON('default theme/'+right+'.json',
+						// loaded
+						function(data){loadTheme(data, false);}
+					);
+					break;
+				case "playlist":
+					getPlaylist(PlayList[right-1].name, 'notPlay');
+					break;
+				case "song":
+					songFromLocation.haveSong = true;
+					songFromLocation.indexSong = right-1;
+					break;
+				case "linksong":
+					songFromLocation.haveSong = true;
+					songFromLocation.indexSong = right;
+					break;
+				case "background":
+					backgNow = right-1;
+					VisualizeGui.backgs = BackList[backgNow].name;
+					backG = loadImage(BackList[backgNow].link);
+					break;
+			}
+		}
+	} else {
+		// background
+		backgNow = floor(random(0, BackList.length));
+		VisualizeGui.backgs = BackList[backgNow].name;
+		backG = loadImage(BackList[backgNow].link);
+
+		// create Audio random
+		VisualizeGui.playlists = PlayList[floor(random([0, 1]))].name;
+		getPlaylist(VisualizeGui.playlists);
+		showFolder('Audio');
+
+		// load theme
+		var nameTheme = random(['HauMaster', 'HoangTran', 'HauMasterLite']);
+		VisualizeGui.themes = nameTheme;
+		loadJSON('default theme/'+nameTheme+'.json',
+			// loaded
+			function(data){loadTheme(data, false);},
+			// error
+			function(){
+				var link = SongList[indexSongNow].link;
+				addAudio(link);
+			}
+		);
+	}
+}
+
+function draw(){
+	if((focused && VisualizeGui.checkFocus) || !VisualizeGui.checkFocus){
+		if(songFromLocation.haveSong){
+			if(isNaN(songFromLocation.indexSong)){
+				addAudio(songFromLocation.indexSong);
+				songFromLocation.haveSong = false;
+
+			} else if(SongList[songFromLocation.indexSong]){
+					addAudio(SongList[songFromLocation.indexSong].link);
+					songFromLocation.haveSong = false;
+			}
+		}
+		animationBackground();
+		autoChangeBackFunc();
+
+		if(second() - mouseActive > 1 && gui.closed) // auto hide dat.GUI
+			gui.domElement.style.display = "none";
+
+		// get data to visualyze
+		if(myAudio){
+			ampLevel = AmpData.getLevel();
+			fftWave = FftData.waveform();
+			fftAnalyze = FftData.analyze();
+			fftAnalyze.splice(65, 1024-64);
+				
+			// run all objects
+			for(var i = 0; i < objects.length; i++)
+				objects[i].run();
+		}
+
+		// choose multi object
+		if(rectChooseMulti.isActive && designMode){
+			rectChooseMulti.show();
+		}
+	}
+}
+
+function keyPressed(){
+	if(keyCode == 83) { // S key
+		designMode = !designMode;
+		VisualizeGui.showDesignMode = designMode;
+		if(designMode) showFolder('Design');
+	
+	} else if(keyCode == LEFT_ARROW){
+		if(myAudio.elt.currentTime >= 5 && !myAudio.elt.paused)
+			myAudio.play().time(myAudio.elt.currentTime-5);
+
+	} else if(keyCode == RIGHT_ARROW && !myAudio.elt.paused){
+		if(myAudio.elt.currentTime < myAudio.elt.duration-5)
+			myAudio.play().time(myAudio.elt.currentTime+5);
+
+	} else if(keyCode == 67) {	// C key
+		if(myAudio){
+			if(!myAudio.elt.controls)
+				myAudio.showControls();
+			else myAudio.hideControls();
+		}
+	}
+}
+
+function mousePressed(){
+	if(designMode){
+		for(var i = 0; i < objects.length; i++)
+			objects[i].boxcontain.mouseChoose();
+
+		// if choose multi object => when click mouse 
+		// => need to check position of mouse
+		// => is mouse position inside any 'choosed' object
+		var foundObjectInChoose = false;
+		for(var i = 0; i < objects.length; i++){
+			if(objects[i].boxcontain.allowChangepos 
+			&& objects[i].boxcontain.chooseMulti){
+				foundObjectInChoose = true;
+				break;
+			}
+		}
+
+		if(!foundObjectInChoose)
+			for(var i = 0; i < objects.length; i++)
+				objects[i].boxcontain.setChooseMulti(false);
+
+	} else {
+		// check if mouse click on a button
+		for(var i = 0; i < objects.length; i++){
+			if(objects[i].objectType == 'ButtonShape')
+				objects[i].clicked();
+		}
+	}
+}
+
+function mouseDragged(){
+	if(designMode){
+		for(var i = 0; i < objects.length; i++)
+			objects[i].boxcontain.drag();
+	
+		if(keyIsDown(CONTROL) && rectChooseMulti.isActive){
+			// save end pos while drag + ctrl
+			rectChooseMulti.setEnd(mouseX, mouseY);
+	
+		} else {
+			rectChooseMulti.setActive(true);
+			// begin and end position will = first pos + ctrl
+			rectChooseMulti.setBegin(mouseX, mouseY);
+			rectChooseMulti.setEnd(mouseX, mouseY);
+		}
+	}
+}
+
+function mouseReleased(){
+	if(rectChooseMulti.isActive && designMode){
+		for(var i = 0; i < objects.length; i++){
+			if(isPointInsideRect(objects[i].pos, rectChooseMulti.beginPoint, rectChooseMulti.endPoint))
+				objects[i].boxcontain.setChooseMulti(true);
+		}
+		rectChooseMulti.setActive(false);
+	}
+}
+
+function mouseMoved() {
+	mouseActive = second();
+	gui.domElement.style.display = "";
+}
+
+function windowResized() {
+	for(var i = 0; i < objects.length; i++){
+		var newPos = createVector(objects[i].pos.x/preWidth*windowWidth, objects[i].pos.y/preHeight*windowHeight);
+		var newSize = createVector(objects[i].size.x/preWidth*windowWidth, objects[i].size.y/preHeight*windowHeight);;
+		objects[i].setPosition(newPos.x, newPos.y);
+		objects[i].setSize(newSize.x, newSize.y);
+		
+		objects[i].boxcontain.applyPosition();
+		objects[i].boxcontain.applySize();
+	}
+	resizeCanvas(windowWidth, windowHeight, true);
+	preWidth = windowWidth;
+	preHeight = windowHeight;
+}
